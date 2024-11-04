d02440a9e0d5de149fa9954b5ea30c6e5d6334de
Jady Lei <jlei50@stuy.edu>
2024-11-04 11:02:24 -0500

    awa

--- app/story_database.py
+++ app/story_database.py
@@ -14,7 +14,7 @@ c = db.cursor() #facilitate db ops
 #     command = "CREATE TABLE IF NOT EXISTS stories (story_id INT AUTO_INCREMENT PRIMARY KEY, story_name)"
 #     c.execute(command)
 def createStories():
-    command = "CREATE TABLE IF NOT EXISTS stories (story_id INT PRIMARY KEY, story_name text NOT NULL, authors, chapters, chapter_count)"
+    command = "CREATE TABLE IF NOT EXISTS stories (story_id INTEGER PRIMARY KEY AUTOINCREMENT, story_name text NOT NULL, chapter_count INT)"
     c.execute(command)
     command = "CREATE TABLE IF NOT EXISTS chapters (story_id INT, chapter_id INT, content text NOT NULL, author, date)"
     c.execute(command)
@@ -25,7 +25,7 @@ def createStories():
 # story_name, author, first_chapter, and date should all be strings.
 def addStory(story_name, author, first_chapter, date):
     # insert story into stories
-    command = "INSERT INTO stories (story_name,chapter_count) VALUES (?,?)"
+    command = "INSERT INTO stories (story_name, chapter_count) VALUES (?,?)"
     val = (story_name,1)
     c.execute(command, val)
     db.commit()
@@ -39,14 +39,18 @@ def addStory(story_name, author, first_chapter, date):
 
 def addChapter(story_id, content, author, date):
     # gets chapter count
-    c.execute("SELECT chapter_count FROM stories WHERE story_id = "+story_id)
-    chapter_count = c.fetchone()
+#     command = "SELECT chapter_count FROM stories WHERE story_id = "+ str(story_id)
+    command = "SELECT * FROM stories "
+    print(command)
+    c.execute(command)
+    chapter_count = c.fetchall()
+    print(chapter_count)
     # add chapter to chapters
     command = "INSERT INTO chapters (story_id, chapter_id, content, author, date) VALUES (?,?,?,?,?)"
     val = (story_id, chapter_count+1, content, author, date)
     c.execute(command, val)
     # update chapter count
-    command = "UPDATE stories SET chapter_count = "+ chapter_count+1 +" WHERE story_id = "+ story_id
+    command = "UPDATE stories SET chapter_count = "+ str(chapter_count+1) +" WHERE story_id = "+ str(story_id)
     c.execute(command)
     db.commit()
     return story_id
@@ -55,23 +59,23 @@ def deleteChapter(story_id, chapter_id):
     # check if either exist
     # TODO
     # delete from chapters
-    command = "DELETE FROM chapters WHERE story_id = " + story_id + "AND" + "chapter_id = " + chapter_id
+    command = "DELETE FROM chapters WHERE story_id = " + str(story_id) + "AND" + "chapter_id = " + str(chapter_id)
     c.execute(command)
     db.commit()
     # update chaptercount for stories
-    c.execute("SELECT chapter_count FROM stories WHERE story_id = "+story_id)
+    c.execute("SELECT chapter_count FROM stories WHERE story_id = "+ str(story_id))
     chapter_count = c.fetchone()
-    command = "UPDATE stories SET chapter_count = " + chapter_count-1 + "WHERE story_id = "+ story_id
+    command = "UPDATE stories SET chapter_count = " + str(chapter_count-1) + "WHERE story_id = "+ str(story_id)
     return story_id
 
 def deleteStory(story_id):
     # check if story exists
     # TODO
     # delete from stories
-    command = "DELETE FROM stories WHERE story_id = " + story_id
+    command = "DELETE FROM stories WHERE story_id = " + str(story_id)
     c.execute(command)
     # delete from chapters
-    c.execute("DELETE FROM chapters WHERE story_id = "+story_id)
+    c.execute("DELETE FROM chapters WHERE story_id = "+ str(story_id))
     db.commit()
     return story_id
 
